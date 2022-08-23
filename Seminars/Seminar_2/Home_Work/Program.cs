d2dbf588437221fe03121e9d9967400c502d6fcf
Ivan <i.klimenko.eng@gmail.com>
2022-08-23 12:47:44 +0400

    third task seminar_2 added

--- Seminars/Seminar_2/Home_Work/Program.cs
+++ Seminars/Seminar_2/Home_Work/Program.cs
@@ -55,6 +55,24 @@ FindThirdNumber(num);
 */
 //Задача №3 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
-//6 -> да
-//7 -> да
-//1 -> нет
+void PrintDayOfWeek(int number)
+{
+     if(number >=1)
+     {
+        if(number <= 5)
+        {
+            Console.WriteLine("Нет");
+        }
+     }
+     if(number <= 7)
+     {
+        if(number >= 6)
+        {
+            Console.WriteLine("Да");   
+        }   
+    }   
+}
+Console.WriteLine("Является ли число, обозначающее день недели, выходным?");
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintDayOfWeek(number);
