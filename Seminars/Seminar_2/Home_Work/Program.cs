6cf0e0d4a4dd0e13e8a0b1baad1ae5cf25b1aa81
Ivan <i.klimenko.eng@gmail.com>
2022-08-25 10:38:31 +0400

    added several solution to second and third tasks

--- Seminars/Seminar_2/Home_Work/Program.cs
+++ Seminars/Seminar_2/Home_Work/Program.cs
@@ -12,7 +12,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 
 PrintSecondNumber(num);
 */
-//Задача №2 Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//Задача №2(решение 1) Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 /*
 void FindThirdNumber(int number)
 {
@@ -53,8 +53,32 @@ Console.Write("Введите положительное число до 100_000
 int num = Convert.ToInt32(Console.ReadLine());
 FindThirdNumber(num);
 */
-//Задача №3 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//Задача №2(решение 2) Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+void FindThirdNumber(int number)
+{
+    if (number >= 100 ^ number <= -100)
+    {
+        int current = number;
+        int result = 0;
+        while(current > 1000 ^ current < -1000)
+        {
+            current /= 10;
+        }
+        result = current % 10;
+        if (result < 0) result = result * (-1);
+        Console.WriteLine($"В числе {number} третья цифра: {result}");
+    } 
+    else Console.WriteLine($"В числе {number} третьей цифры нет");
+}
+
+Console.WriteLine("Программа для вывода третьей цифры заданного числа");
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+FindThirdNumber(num);
+
+//Задача №3(решение 1) Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
 void PrintDayOfWeek(int number)
 {
      if(number >=1)
@@ -76,3 +100,22 @@ Console.WriteLine("Является ли число, обозначающее д
 Console.Write("Введите число от 1 до 7: ");
 int number = Convert.ToInt32(Console.ReadLine());
 PrintDayOfWeek(number);
+*/
+//Задача №3(решение 2) Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+void PrintDayOfWeek(int number)
+{
+if(number > 0 & number < 6)
+    {            
+        Console.WriteLine("Нет");
+    }
+if(number >= 6 & number <= 7)
+    {
+    Console.WriteLine("Да");   
+    }  
+}
+Console.WriteLine("Является ли число, обозначающее день недели, выходным?");
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+PrintDayOfWeek(number);
+*/
