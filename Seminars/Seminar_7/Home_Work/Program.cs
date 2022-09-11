715874facc59fa1c215f0fff65b02fd1b6336f4e
Ivan <i.klimenko.eng@gmail.com>
2022-09-12 00:03:38 +0400

    Home Work Seminar 7 added

--- Seminars/Seminar_7/Home_Work/Program.cs
+++ Seminars/Seminar_7/Home_Work/Program.cs
@@ -38,46 +38,83 @@ double[,] newArray = CreateRandom2dArray();
 Show2dArray(newArray);
 */
 //Задача №2 - Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 17 -> такого числа в массиве нет
+//Решение 1 - Если задан массив
+/*
+int[,] myArray = { { 1, 4, 7, 2, }, 
+                   { 5, 9, 2, 3 }, 
+                   { 8, 4, 2, 4 } };
 
-int[,] myArray = { { 1, 4, 7, 2, }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+void FindElementArray(int indexI, int indexJ)
+{
+    if(indexI > myArray.GetLength(0) - 1 || indexJ > myArray.GetLength(1)- 1)
+        Console.WriteLine($"Элемента с позицией [{indexI},{indexJ}] нет в массиве.");
+    for (int i = 0; i < myArray.GetLength(0); i++)
+        for (int j = 0; j < myArray.GetLength(1); j++)
+            if(i==indexI && j==indexJ) 
+                Console.WriteLine($"Значение элемента с позицией [{indexI},{indexJ}]: {myArray[i,j]}");          
+}
 
+Console.Write("Введите позицию элемента в строке: ");
+int indexI = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию элемента в столбце: ");
+int indexJ = Convert.ToInt32(Console.ReadLine());
+FindElementArray(indexI, indexJ);
+*/
+//Решение 2 - Универсальный метод
+/*
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input min possitible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input max possitible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+        
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)   
+        for(int j = 0; j < columns; j++)   
+            newArray[i,j] = new Random().Next(minValue, maxValue + 1); 
+
+    return newArray; 
+}
 
 void Show2dArray(int[,] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write(array[i, j] + " ");
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
 
-        Console.WriteLine();
+        Console.WriteLine();    
     }
 
     Console.WriteLine();
 }
+
 void FindElementArray(int[,] array, int indexI, int indexJ)
 {
+    if(indexI > array.GetLength(0) - 1 || indexJ > array.GetLength(1)- 1)
+        Console.WriteLine($"Элемента с позицией [{indexI},{indexJ}] нет в массиве.");
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.WriteLine($"Значение элемента с индексами [{indexI},{indexJ}]: {array[indexI,indexJ]}");
-            break;
-            //if(i==indexI || j==indexJ) Console.WriteLine($"Значение элемента с индексами [{indexI},{indexJ}]: {array[i,j]}");
-            //if(i != indexI || j != indexJ) Console.WriteLine($"{indexJ} --> такого числа в массиве нет");
-        }
-            
-                   
+            if(i==indexI && j==indexJ) 
+                Console.WriteLine($"Значение элемента с позицией [{indexI},{indexJ}]: {array[i,j]}");          
 }
-Show2dArray(myArray);
-Console.Write("imput index [i]: ");
+int[,] newArray = CreateRandom2dArray();
+Show2dArray(newArray);
+
+Console.Write("Введите позицию элемента в строке: ");
 int indexI = Convert.ToInt32(Console.ReadLine());
-Console.Write("imput index [j]: ");
+Console.Write("Введите позицию элемента в столбце: ");
 int indexJ = Convert.ToInt32(Console.ReadLine());
-FindElementArray(myArray, indexI, indexJ);
+
+FindElementArray(newArray, indexI, indexJ);
+*/
+
 
 
 //Задача №3 - Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
@@ -86,3 +123,43 @@ FindElementArray(myArray, indexI, indexJ);
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();    
+    }
+
+    Console.WriteLine();
+}
+
+void FindAverageSum(int[,] array)
+{
+    double sum = 0;
+    double avarSum = 0;
+    Console.Write("Среднее арифметическое каждого столбца: ");
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        for(int j = 0; j < array.GetLength(0); j++)
+            {
+                sum += array[j,i];
+            }
+        
+        avarSum = Math.Round((sum / array.GetLength(0)) , 2);
+        sum = 0;
+        Console.Write(avarSum + " ");
+        avarSum = 0;
+    }
+              
+}
+int[,] myArray =  {{ 1, 4, 7, 2,}, 
+                   { 5, 9, 2, 3 }, 
+                   { 8, 4, 2, 4 }};
+
+
+Show2dArray(myArray);
+FindAverageSum(myArray);
