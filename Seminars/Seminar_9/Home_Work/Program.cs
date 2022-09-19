776700192de0a61a0076e005672529fabb506551
Ivan <i.klimenko.eng@gmail.com>
2022-09-20 00:03:19 +0300

    Home Work of Seminar 9 added

--- Seminars/Seminar_9/Home_Work/Program.cs
+++ Seminars/Seminar_9/Home_Work/Program.cs
@@ -1,22 +1,38 @@
-﻿//Задача № 1 - Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
-// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
-// M = 4; N = 8. -> ""4, 6, 7, 8""
-
-void PrintDiapNum(int numM, int numN)
+﻿
+//Задача №1 - Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
+int FindSum(int m, int n)
 {
-    if(numM <= numN)
+    int sum = 0;
+    if (n >= m)
     {
-        Console.Write(numM + 1);
-        PrintDiapNum(numM, numN);
+        if (n >= m)
+            sum = FindSum(m + 1, n);
+        return sum += m;
     }
+    return sum;
+}
 
-    }
-        
-
-PrintDiapNum(1, 5);
-//Задача №2 - Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
-
+Console.Write("Imput of number M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput of number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int sum = FindSum(numberM, numberN);
+Console.WriteLine("The sum of the numbers from M to N: " + sum);
+*/
 //Задача №3 - Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 29
+
+int FunctionOfAckerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+        else if (m != 0 && n == 0) return FunctionOfAckerman(m - 1, 1);
+            else return FunctionOfAckerman(m - 1, FunctionOfAckerman(m, n - 1));
+}
+
+Console.Write("Imput of number M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput of number N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int resultFunction = FunctionOfAckerman(numberM, numberN);
+Console.WriteLine($"The value of the Ackerman function A({numberM},{numberN}): {resultFunction}");
+
