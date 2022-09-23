3862033b2d19eeec97c3eb11012fa89bc2ce5e1b
Ivan <i.klimenko.eng@gmail.com>
2022-09-23 12:16:37 +0400

    Home Work SeminarWeb_2 added

--- Seminars/SeminarWeb_2/Program.cs
+++ Seminars/SeminarWeb_2/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿
+using static System.Console;
 using System;
  //Tasl 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 // 456 -> 5
@@ -15,9 +16,29 @@ WriteLine($"Вторая цифра числа {number} -> {dec}");
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+/*
+Clear();
+Write("Введите число: от -1000000 до 1000000: ");
+int number = int.Parse(ReadLine());
+if(number > -100 && number < 100) WriteLine($"{number} -> третьей цифры нет");
+int hud = number / 10;
+if (number >= 100 && number < 1000) Write($"{number} -> {number % 10}");
+if (number >= 1000 && number < 10000) Write($"{number} -> {hud % 10}");
+if (number >= 10000 && number < 100000) Write($"{number} -> {(hud / 10) % 10}");
+if (number >= 100000 && number < 1000000) Write($"{number} -> {(hud / 100) % 10}");
+if (number >= -1000 && number <= -100) Write($"{number} -> {number % 10 * (-1)}");
+if (number >= -10000 && number < -1000) Write($"{number} -> {hud % 10 * (-1)}");
+if (number >= -100000 && number < -10000) Write($"{number} -> {(hud / 10) % 10 * (-1)}");
+if (number >= -1000000 && number < -100000) Write($"{number} -> {(hud / 100) % 10 * (-1)}");
+*/
+//Task 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
 
-Console.Clear();
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-if(number < 100 || number > -100) WriteLine($"{number} -> третьей цифры нет");
-while(number < 1000)
+Clear();
+Write("Введите цифру дня недели: ");
+int dayOfWeek = int.Parse(ReadLine());
+if(dayOfWeek == 6 || dayOfWeek == 7) Write($"{dayOfWeek} -> Да");
+if(dayOfWeek > 0 && dayOfWeek < 6) Write($"{dayOfWeek} -> Нет");
+if(dayOfWeek <= 0 || dayOfWeek > 7) Write("Введен неверный день недели");
